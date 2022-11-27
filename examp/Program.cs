a65f94f17413b5267950afe9dad87017302958bd
Svetlana <nooch@yandex.ru>
2022-11-27 23:55:44 +0300

    программа на вывод нового массива

---
+++ examp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string[] array = new string [4] {"hello", "2", "word", ":-)"};
+string[] newArray = new string[array.Length];
+
+void Array(string[] array)
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newArray[index] = array[i];
+        index++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(null != array[i])
+        {   
+            Console.Write($"{array[i]},\t");
+        }
+    }
+}
+
+Console.Write($"из исходного массива -> ");
+Array(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($" -> получаем массив массив строк ->");
+PrintArray(newArray);
+
